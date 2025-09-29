f6f1aa8fb9bde041f7075c8cdf5249c8d2c3e5af
MaxenceChan <123729633+MaxenceChan@users.noreply.github.com>
2025-09-29 12:39:11 +0200

    Delete src/components/ui directory

--- src/components/ui/label.jsx
+++
@@ -1,6 +0,0 @@
-
-import React from 'react'
-
-export function Label({ className = '', children, ...props }) {
-  return <label className={`text-sm text-gray-700 ${className}`} {...props}>{children}</label>
-}
